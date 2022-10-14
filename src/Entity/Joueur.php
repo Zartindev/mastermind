986aa1f902d5d6bf76f36c56490ce2541a4eb115
Zartinx <martinguillon.mgcode@gmail.com>
2022-10-14 14:51:44 +0200

    Création de registration et login form

--- src/Entity/Joueur.php
+++ src/Entity/Joueur.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\JoueurRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: JoueurRepository::class)]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
 class Joueur implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
